e9645afa134a2fa3669995c3f345cb6448d9abd5
keshariamudit24 <keshariamudit@gmail.com>
2025-06-13 15:22:54 +0530

    schemas defined

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
 const app = express();
+const PORT = process.env.PORT
 
 app.use(json());
 
-app.listen(3000);
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => { app.listen(PORT, () => { console.log(`listening on port: ${PORT}.....`) }) })
+    .catch()
 
 // routes 
 app.use("/user", userRoute);
